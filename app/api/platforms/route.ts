c3af5da3b88bda9eb6fe147cf6e118712179863b
Arpit Sharma <officialarpit7@gmail.com>
2026-06-18 21:04:05 +0530

    enforcing github activity

---
+++ app/api/platforms/route.ts
@@ -0,0 +1,40 @@
+import {NextResponse,NextRequest} from "next/server";
+import { prisma } from "@/app/generated/prisma";
+import { createClient } from "@/utils/supabase/server";
+export async function POST(req:NextRequest){
+    try{
+        const supabase=await createClient();
+        const{
+            data:{user},
+            error,
+        }=await supabase.auth.getUser();
+        if(error || !user){
+            return NextResponse.json(
+                {error:"unauthorized"},
+                {status:411}
+            )
+        }
+        const body=await req.json();
+        const {name,meta,url}=body;
+        const platform=await prisma.platform.create({
+            data:{
+                userId:user.id,
+                meta,
+                Pname:name,
+                Purl:url,
+                isConnected:true
+            }
+        })
+        return NextResponse.json({
+            message:"success"
+        },
+        {status:201}
+    );
+} catch(error){
+    return NextResponse.json({
+        error:"Failed to create platform"
+    },{
+        status:500
+    }
+)
+} }
